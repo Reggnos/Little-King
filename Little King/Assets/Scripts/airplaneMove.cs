4a1df2625677b33d1b310ca69cf35ea681f9580d
DESKTOP-TANB250\Reggnos <stankovicmateja@yahoo.com>
2018-07-28 15:20:58 +0200

    Added game do git

---
+++ Little King/Assets/Scripts/airplaneMove.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class airplaneMove : MonoBehaviour {
+
+    public float speedForward = 5.0f;
+    public float speedMove = 5.0f;
+    public float startTimeSlow = 8.0f;      //when does the airplane start to get slower
+    public float intervalsForSlow = 1.0f;   //how many seconds does it need to get slower again
+
+    private float timeBetweenIntervals;    //the amount of the that has passed since the last slow has gone through
+    private float amountOfSlow = 0.5f;  
+    private float horizontalInput;
+    private float timeFromStart;
+    private Vector3 forwardVector = new Vector3(0, 0, 1);
+    private Vector3 moveVector = new Vector3(1, 0, 0);
+    Rigidbody rb;
+   
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        timeBetweenIntervals = intervalsForSlow;
+        
+     }
+
+    // Update is called once per frame
+    void Update() {
+
+        timeFromStart = Time.fixedTime;
+        horizontalInput = Input.GetAxisRaw("Horizontal"); 
+
+        rb.transform.Translate(forwardVector * speedForward * Time.deltaTime);  //Forward movement
+        
+        rb.AddForce(moveVector * horizontalInput * speedMove); //Horizontal movement
+
+        timeBetweenIntervals += Time.deltaTime;
+
+        if (timeFromStart > startTimeSlow && intervalsForSlow <= timeBetweenIntervals && speedMove >= 0)   //Movement Slow
+        {
+            timeBetweenIntervals = 0.0f;
+            speedMove -= amountOfSlow;
+            
+        }
+
+
+
+
+
+        Debug.Log(timeFromStart);
+
+
+	}
+}
---
+++ Little King/Assets/Scripts/airplaneRotation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class airplaneRotation : MonoBehaviour {
+
+    Transform objectTrans;
+
+    void Start () {
+        objectTrans = GetComponent<Transform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        if (horizontalInput > 0 && objectTrans.rotation.z > -0.3f)   // rotation while movement
+        {
+            objectTrans.Rotate(new Vector3(0, 0, -1));
+        }
+        else if (horizontalInput < 0 && objectTrans.rotation.z < 0.3f)
+        {
+            objectTrans.Rotate(new Vector3(0, 0, 1));
+        }
+        else if (horizontalInput == 0)
+            if (objectTrans.rotation.z < -0.01f || objectTrans.rotation.z > 0.01f)
+            {
+                if (objectTrans.rotation.z < 0.0f)
+                    objectTrans.Rotate(new Vector3(0, 0, 1) * 0.5f);
+                else if (objectTrans.rotation.z > 0.0f)
+                    objectTrans.Rotate(new Vector3(0, 0, -1) * 0.5f);
+            }
+        
+    }
+}
